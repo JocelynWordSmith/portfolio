629beaa1451dc5a6590923371ac957464f1b9f26
Joshua Smith <joshuawordsmith@gmail.com>
2018-07-04 15:19:43 -0400

    updated work and school history, updated styles

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
-$(window).on('beforeunload', function() {
-    $(window).scrollTop(0);
-});
- console.log("There shouldn't be any errors down here, but if there are they were totally on purpose. And while I didn't add it to my portfolio, I made this site by building on a Bootstrap template. The space and water images are from unsplash.com, but I made pretty much all the icons and the top logo. Cheers. -Joshua Smith")
+// $(window).on('beforeunload', function() {
+//     // $(window).scrollTop(0);
+// });
+//  console.log("There shouldn't be any errors down here, but if there are they were totally on purpose. And while I didn't add it to my portfolio, I made this site by building on a Bootstrap template. The space and water images are from unsplash.com, but I made pretty much all the icons and the top logo. Cheers. -Joshua Smith")
 
 // jQuery to collapse the navbar on scroll
 $(window).scroll(function() {
@@ -47,7 +47,6 @@ $(document).ready(function(){
         var buff = $(window).width()/8;
         shipM = (shipX - buff);
         $('#ship').css('margin-left', shipM );
-
     })
 
     $('.mod-link').click(function(){
@@ -59,170 +58,63 @@ $(document).ready(function(){
             $('#bullet').remove()
           });
     })
+
+    processJSONContent();
 });
 
+const insertAfter = function(newNode, referenceNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+};
 
-// var mouseX = 0, mouseY = 0, limitX = 150-15, limitY = 150-15;
-// $(window).mousemove(function(e){
-//  mouseX = Math.min(e.pageX, limitX);
-//  mouseY = Math.min(e.pageY, limitY);
-// });
+const domSwap = function(nodeString, referenceNode) {
+    const tempNode = document.createElement('span');
+    tempNode.innerHTML = nodeString;
+    const newNode = tempNode.firstElementChild;
+    insertAfter(newNode, referenceNode);
+};
 
-// // cache the selector
-// var follower = $("#ship");
-// var xp = 0, yp = 0;
-// var loop = setInterval(function(){
-// // change 12 to alter damping higher is slower
-// xp += (mouseX - xp) / 12;
-// yp += (mouseY - yp) / 12;
-// follower.css({left:xp, top:yp});
 
-// }, 30);
+const injectInDom = function(item) {
+    [].forEach.call(document.querySelectorAll('[data-replace="' + item.type + '"]'), (element) => {
+        domSwap(item.content, element);
+    });
+};
 
-// // Google Maps Scripts
-// // When the window has finished loading create our google map below
-// google.maps.event.addDomListener(window, 'load', init);
+const makeTag = function(key) {
+    return '{{' + key + '}}';
+};
 
-// function init() {
-//     // Basic options for a simple Google Map
-//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-//     var mapOptions = {
-//         // How zoomed in you want the map to start at (always required)
-//         zoom: 15,
+const iterateTemplateContent = function(wrapper, templateData, templateChunk) {
+    const chunkString = templateChunk.outerHTML;
+    templateData.forEach(function(content) {
+        let stringCopy = chunkString;
+        Object.keys(content).forEach(function(contentKey) {
+            stringCopy = stringCopy.replace(makeTag(contentKey), content[contentKey]);
+        });
+        stringCopy.replace('data-iterate', '');
+        domSwap(stringCopy, templateChunk);
+    });
+    templateChunk.remove();
+};
 
-//         // The latitude and longitude to center the map (always required)
-//         center: new google.maps.LatLng(40.6700, -73.9400), // New York
+const renderTemplate = function(templateData) {
+    const wrapper= document.createElement('div');
+    wrapper.innerHTML=  document.querySelector('[data-template="' + templateData.dataTemplate + '"]').innerHTML;
+    Object.keys(templateData.content).forEach(function(contentKey) {
+        const templateChunk = wrapper.querySelector('[data-iterate="' + contentKey + '"]');
+        iterateTemplateContent(wrapper, templateData.content[contentKey], templateChunk);
+    });
+    [].forEach.call(document.querySelectorAll('[data-replace="' + templateData.dataTemplate + '"]'), (element) => {
+        insertAfter(wrapper, element);
+    });
+};
 
-//         // Disables the default Google Maps UI components
-//         disableDefaultUI: true,
-//         scrollwheel: false,
-//         draggable: false,
-
-//         // How you would like to style the map. 
-//         // This is where you would paste any style found on Snazzy Maps.
-//         styles: [{
-//             "featureType": "water",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }]
-//         }, {
-//             "featureType": "landscape",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 20
-//             }]
-//         }, {
-//             "featureType": "road.highway",
-//             "elementType": "geometry.fill",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }]
-//         }, {
-//             "featureType": "road.highway",
-//             "elementType": "geometry.stroke",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 29
-//             }, {
-//                 "weight": 0.2
-//             }]
-//         }, {
-//             "featureType": "road.arterial",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 18
-//             }]
-//         }, {
-//             "featureType": "road.local",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 16
-//             }]
-//         }, {
-//             "featureType": "poi",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 21
-//             }]
-//         }, {
-//             "elementType": "labels.text.stroke",
-//             "stylers": [{
-//                 "visibility": "on"
-//             }, {
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 16
-//             }]
-//         }, {
-//             "elementType": "labels.text.fill",
-//             "stylers": [{
-//                 "saturation": 36
-//             }, {
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 40
-//             }]
-//         }, {
-//             "elementType": "labels.icon",
-//             "stylers": [{
-//                 "visibility": "off"
-//             }]
-//         }, {
-//             "featureType": "transit",
-//             "elementType": "geometry",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 19
-//             }]
-//         }, {
-//             "featureType": "administrative",
-//             "elementType": "geometry.fill",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 20
-//             }]
-//         }, {
-//             "featureType": "administrative",
-//             "elementType": "geometry.stroke",
-//             "stylers": [{
-//                 "color": "#000000"
-//             }, {
-//                 "lightness": 17
-//             }, {
-//                 "weight": 1.2
-//             }]
-//         }]
-//     };
-
-    // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
-    // var mapElement = document.getElementById('map');
-
-    // Create the Google Map using out element and options defined above
-    // var map = new google.maps.Map(mapElement, mapOptions);
-
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-//     var image = 'img/map-marker.png';
-//     var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-//     var beachMarker = new google.maps.Marker({
-//         position: myLatLng,
-//         map: map,
-//         icon: image
-//     });
-// }
+const processJSONContent = function() {
+    dataReplace.forEach((item) => {
+        if (item.dataTemplate) {
+            renderTemplate(item);
+        } else {
+            injectInDom(item);
+        }
+    });
+};
---
+++ js/template-copy/copy.js
@@ -0,0 +1,95 @@
+window.dataReplace = [{
+        "type": "contact-link",
+        "content": "<a href=\"mailto:joshuawordsmith@gmail.com\" class=\"btn btn-default btn-lg\"><i class=\"fa fa-envelope fa-fw\"></i><span class=\"network-name\">Contact Josh</span></a>"
+    }, {
+        "type": "close-button",
+        "content": "<button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Close</button>"
+    }, {
+        "dataTemplate": "navbar-main-collapse",
+        "content": {
+            "listItem": [{
+                "href": "about",
+                "text": "About"
+            }, {
+                "href": "portfolio",
+                "text": "Portfolio"
+            }, {
+                "href": "work-history",
+                "text": "Work History"
+            }, {
+                "href": "contact",
+                "text": "Contact"
+            }]
+        }
+    }, {
+        "dataTemplate": "contact-section",
+        "content": {
+            "listItem": [{
+                "href": "mailto:joshuawordsmith@gmail.com",
+                "icon": "envelope",
+                "text": "Email"
+            }, {
+                "href": "https://github.com/JoshuaWordSmith",
+                "icon": "github",
+                "text": "Github"
+            }, {
+                "href": "https://twitter.com/Joshuawordsmith",
+                "icon": "twitter",
+                "text": "Twitter"
+            }],
+            "signoff": [{
+                "header": "Contact Joshua Smith",
+                "copy1": "Whether you liked anything enough to talk about working together, or hated anything enough that you need to let me know, I look forward to hearing from you.",
+                "email": "JoshuaWordSmith@gmail.com",
+                "emailHref": "mailto:joshuawordsmith@gmail.com",
+                "copy2": "References and sweet drum solos available upon request."
+            }]
+        }
+    }, {
+        "dataTemplate": "skills-modal",
+        "content": {
+            "skillIcon": [{
+                "src": "img/bow.png",
+                "text": "HTML5/CSS3"
+            }, {
+                "src": "img/boomerang.png",
+                "text": "Underscore"
+            }, {
+                "src": "img/bomb.png",
+                "text": "JavaScript\nES6"
+            }, {
+                "src": "img/lantern.png",
+                "text": "Backbone"
+            }, {
+                "src": "img/net.png",
+                "text": "jQuery"
+            }, {
+                "src": "img/heart-container.png",
+                "text": "Redux"
+            }, {
+                "src": "img/boss-key.png",
+                "text": "Less/SCSS"
+            }, {
+                "src": "img/sword-down.png",
+                "text": "Dust.js"
+            }, {
+                "src": "img/chest.png",
+                "text": "Handlebars"
+            }, {
+                "src": "img/red-pot.png",
+                "text": "iTerm/Zsh"
+            }, {
+                "src": "img/green-pot.png",
+                "text": "Sourcetree"
+            }, {
+                "src": "img/blue-pot.png",
+                "text": "Git/Github"
+            }, {
+                "src": "img/sheild.png",
+                "text": "ADA"
+            }, {
+                "src": "img/gem.png",
+                "text": "Node"
+            }]
+        }
+    }];
